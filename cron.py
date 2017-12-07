451657f858121a33d128cea791cf421e4a37e2b9
Richard Reijmers <r.reijmers@cdn.leaseweb.com>
2017-12-07 22:03:10 +0100

    Some if that doesn't work yet

--- cron.py
+++ cron.py
@@ -9,24 +9,28 @@ conn = sqlite3.connect('values.db')
 c = conn.cursor()
 
 # Create some tables if they don't exist already
-c.execute('''CREATE TABLE IF NOT EXISTS crypto
-             (date, pair, ask, bid)''')
+#c.execute('''CREATE TABLE IF NOT EXISTS crypto
+#             (date, pair, ask, bid)''')
 c.execute('''CREATE TABLE IF NOT EXISTS transactions
-             (date, pair, ask, bid)''')
+             (date, pair, ask, bid, amount, saldo)''')
 # Get the last transaction to see if anything is stored on the pair
-c.execute('SELECT * FROM transactions WHERE pair=? ORDER BY date LIMIT 1', pair)
-lasttransaction = c.fetchone()
-
+c.execute('SELECT bid FROM transactions WHERE pair=? ORDER BY date LIMIT 1', pair)
+lastbidvalue = c.fetchone()
+print(lastbidvalue)
 # If there is no last transaction, query it and insert it.
-if lasttransaction:
-    print(lasttransaction)
+if lastbidvalue:
+    print(lastbidvalue)
 else:
     bought = input("What was the price you bought XLTCZEUR for? ")
     datebought = input("When did you buy XLTCZEUR? (YYYY-MM-DD) ")
-    c.execute("INSERT INTO transactions VALUES ('"+datebought+"','XLTCZEUR',"+bought+","+bought+")")
-    c.execute('SELECT * FROM transactions WHERE pair=? ORDER BY date LIMIT 1', pair)
-    lasttransaction = c.fetchone()
-    print(lasttransaction)
+    amountbought = input("How much XLTCZEUR did you buy? ")
+    saldo = input("What's your current saldo on XLTCZEUR? ")
+    c.execute("INSERT INTO transactions VALUES ('"+datebought+"','XLTCZEUR',"+bought+","+bought+","+amountbought+","+saldo+")")
+    c.execute('SELECT bid FROM transactions WHERE pair=? ORDER BY date LIMIT 1', pair)
+    conn.commit()
+
+lastbidvalue = c.fetchone()
+print(lastbidvalue)
 
 # Some API call practice, still needs to be worked out.
 with urllib.request.urlopen("https://api.kraken.com/0/public/Ticker?pair=XLTCZEUR") as url:
@@ -36,3 +40,12 @@ with urllib.request.urlopen("https://api.kraken.com/0/public/Ticker?pair=XLTCZEU
     ltceurbid = data['result']['XLTCZEUR']['b'][0]
     print(ltceurask)
     print(ltceurbid)
+
+print(lastbidvalue)
+# If current price is higher
+if ltceurbid > lastbidvalue:
+    print("higher")
+else:
+    print("lower")
+
+conn.close()
