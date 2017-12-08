2f3f69c18b20de434cb5202ccea21595787405c0
Richard Reijmers <r.reijmers@cdn.leaseweb.com>
2017-12-09 00:20:12 +0100

    Added comments/documentation

--- cron.py
+++ cron.py
@@ -2,42 +2,46 @@
 
 import urllib.request, json, sqlite3, datetime, sys, configparser
 
+# Get some variables outside this script
 config = configparser.ConfigParser()
 config.read('./config.txt')
 crypto = config['CURRENCIES']['CRYPTO']
 fiat = config['CURRENCIES']['FIAT']
 pair = "X" + crypto + "Z" + fiat
 
-print(pair)
-
+# Define some variables
 today = str(datetime.date.today())
 now = str(datetime.datetime.now())
 
+# Connect to SQLite3
 conn = sqlite3.connect('values.db')
 c = conn.cursor()
 
-#c.execute('''CREATE TABLE IF NOT EXISTS transactions
-#             (date, pair, ask, bid, amount)''')
+# Create some tables if they don't exist already
 c.execute('''CREATE TABLE IF NOT EXISTS '''+crypto.lower()+'''
              (date, balance, ask, bid)''')
 c.execute('''CREATE TABLE IF NOT EXISTS '''+fiat.lower()+'''
              (date, balance, ask, bid)''')
 
+# Get latest crypto balance
 c.execute('SELECT balance FROM '+crypto.lower()+' ORDER BY date DESC LIMIT 1')
 lastcryptobalance = c.fetchone()
 if lastcryptobalance:
     lastcryptobalancefloat = float(lastcryptobalance[0])
 
+# Get latest fiat balance
 c.execute('SELECT balance FROM '+fiat.lower()+' ORDER BY date DESC LIMIT 1')
 lastfiatbalance = c.fetchone()
 if lastfiatbalance:
     lastfiatbalancefloat = float(lastfiatbalance[0])
 
+# Get the currect fiat value for this crypto
 with urllib.request.urlopen("https://api.kraken.com/0/public/Ticker?pair=" + pair) as url:
     data = json.loads(url.read().decode())
     cryptoaskprice = float(data['result'][pair]['a'][0])
     cryptobidprice = float(data['result'][pair]['b'][0])
 
+# Check if you already have a crypto balance higher than 0.
 if lastcryptobalance and lastcryptobalancefloat != 0:
     print("You have " + crypto + " balance (" + str(lastcryptobalancefloat) + "), you want to check if the value is lower and possibly sell")
     nolastcryptobalance = False
@@ -45,6 +49,7 @@ if lastcryptobalance and lastcryptobalancefloat != 0:
     c.execute('SELECT ask FROM '+crypto.lower()+' ORDER BY date DESC LIMIT 1')
     lastcryptoaskprice = float(c.fetchone()[0])
 
+    # Check if the bid price is higher than the price you have bought your crypto for
     if cryptobidprice > lastcryptoaskprice:
         print("Current " + crypto + " bid price (" + str(cryptobidprice) + ") is higher than the price that you bought (ask) it for (" + str(lastcryptoaskprice) + ")")
         print("Would probably keep")
@@ -60,6 +65,7 @@ if lastcryptobalance and lastcryptobalancefloat != 0:
 else:
     nolastcryptobalance = True
 
+# Check if you already have a fiat balance higher than 0.
 if lastfiatbalance and lastfiatbalancefloat != 0:
     print("You have " + fiat + " balance (" + str(lastfiatbalancefloat) + "), you want to check if the value is higher and possibly buy")
     nolastfiatbalance = False
@@ -67,6 +73,7 @@ if lastfiatbalance and lastfiatbalancefloat != 0:
     c.execute('SELECT bid FROM '+fiat.lower()+' ORDER BY date DESC LIMIT 1')
     lastfiatbidprice = float(c.fetchone()[0])
 
+    # Check if the ask price is higher than the price you have sold your crypto for
     if cryptoaskprice > lastfiatbidprice:
         print("Current " + crypto + " ask price (" + str(cryptoaskprice) + ") is higher than the price that you sold (bid) it for (" + str(lastfiatbidprice) + ")")
         print("Would probably buy")
@@ -82,6 +89,7 @@ if lastfiatbalance and lastfiatbalancefloat != 0:
 else:
     nolastfiatbalance = True
 
+# If you neither have a crypto balance, nor a fiat balance, you're poor and need to fill in some questions
 if nolastcryptobalance == nolastfiatbalance == True:
     print("You have neither " + crypto + " or " + fiat + " balance")
     currencychoice = input("Which currency currently holds balance (" + crypto.lower() + "/" + fiat.lower() + ")? ")
@@ -100,60 +108,12 @@ if nolastcryptobalance == nolastfiatbalance == True:
             print("No valid choice, please type (y/n).")
             sys.exit()
         c.execute("INSERT INTO "+crypto.lower()+" VALUES ('" + datetimebought + "'," + str(cryptobalance) + "," + str(cryptoaskprice) + "," + str(cryptobidprice) + ")")
+        conn.commit()
     elif currencychoice.lower() == fiat.lower():
         print("Entering " + fiat + " balance")
     else:
         print("No valid choice")
         sys.exit()
 
-
-
-## Get the last transaction to see if anything is stored on the pair
-#c.execute('SELECT bid FROM transactions WHERE pair=? ORDER BY date LIMIT 1', pairtuple)
-#lastbidvalue = c.fetchone()
-##print(lastbidvalue[0])
-## If there is no last transaction, query it and insert it.
-#if lastbidvalue:
-#    print("Already got a latest transaction for " + pairstr)
-#    lastbidvalueclean = float(lastbidvalue[0])
-#else:
-#    bought = input("What was the price you bought " + pairstr + " for? ")
-#    datebought = input("When did you buy " + pairstr + "? (YYYY-MM-DD) ")
-#    amountbought = input("How much " + pairstr + " did you buy? ")
-#    saldo = input("What's your current saldo on " + pairstr + "? ")
-#    c.execute("INSERT INTO transactions VALUES ('"+datebought+"'," + pairstr + ","+bought+","+bought+","+amountbought+")")
-#    conn.commit()
-#    c.execute('SELECT bid FROM transactions WHERE pair=? ORDER BY date LIMIT 1', pairtuple)
-#    lastbidvalueclean = float(c.fetchone()[0])
-#
-#print("Last transaction bid value: " + str(lastbidvalueclean))
-#
-## Some API call practice, still needs to be worked out.
-#with urllib.request.urlopen("https://api.kraken.com/0/public/Ticker?pair=XLTCZEUR") as url:
-#    data = json.loads(url.read().decode())
-#    #print(json.dumps(data, sort_keys=True, indent=4))
-#    ltceurask = float(data['result']['XLTCZEUR']['a'][0])
-#    ltceurbid = float(data['result']['XLTCZEUR']['b'][0])
-#    print(pairstr + " ask price: " + str(ltceurask))
-#    print(pairstr + " bid price: " + str(ltceurbid))
-#
-#
-##print(lastbidvalue[0])
-##
-## If current price is higher
-#if ltceurbid > lastbidvalueclean:
-#    print("Current value is higher, wouldn't sell")
-#else:
-#    print("Current value is lower, would sell")
-#    c.execute('SELECT saldo FROM transactions WHERE pair=? ORDER BY date LIMIT 1', pairtuple)
-#    lastsaldo = float(c.fetchone()[0])
-#    saldofiatvalue = lastsaldo * ltceurbid
-#    print("Your LTC saldo of " + str(lastsaldo) + " would sell for " + str(saldofiatvalue) + " EUR")
-#    c.execute("INSERT INTO eur VALUES ('"+today+"',"+str(saldofiatvalue)+")")
-#    conn.commit()
-#
-#
-#
-
-conn.commit()
+# Close the SQLite3 connection
 conn.close()
